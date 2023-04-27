3f6ac9eed556c10d47dbbfb94b416c9d2e66ea12
helmenaemmiu <hossammaa.ahmed@miu.edu>
2023-04-27 16:58:26 -0500

    add the fav functionalities

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react';
-import NavBar from './components/nav';
-import PageRoutes from './routes/routes';
-import { BrowserRouter } from 'react-router-dom';
-import { SetJWT } from './store/context';
-import Cookies from 'js-cookie';
-import jwt_decode from 'jwt-decode';
+import { useEffect, useState } from "react";
+import NavBar from "./components/nav";
+import PageRoutes from "./routes/routes";
+import { BrowserRouter } from "react-router-dom";
+import { SetJWT } from "./store/context";
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 function App() {
   const [jwt, setJwt] = useState("");
   const [useID, setUserID] = useState("");
   const [userRole, setUserRole] = useState("");
 
-
   useEffect(() => {
     if (Cookies.get("accessToken")) {
-      setJwt(jwt_decode(Cookies.get("accessToken")));
-      setUserRole(jwt_decode(Cookies.get("accessToken")).sub.split(',')[1]);
-      setUserID(jwt_decode(Cookies.get("accessToken")).sub.split(',')[2]);
+      setJwt(Cookies.get("accessToken"));
+      setUserRole(jwt_decode(Cookies.get("accessToken")).sub.split(",")[1]);
+      setUserID(jwt_decode(Cookies.get("accessToken")).sub.split(",")[2]);
     }
   }, [Cookies.get("accessToken")]);
 
   return (
     <div className="App">
-      <SetJWT.Provider value={{ jwt, setJwt, useID, setUserID, userRole, setUserRole }} >
+      <SetJWT.Provider
+        value={{ jwt, setJwt, useID, setUserID, userRole, setUserRole }}
+      >
         <BrowserRouter>
           <NavBar />
           <PageRoutes />
--- src/components/property/popup/index.jsx
+++ src/components/property/popup/index.jsx
@@ -1,33 +1,91 @@
 import { Button, Input } from "../../shared/style";
-import { AddIcon, Content, Header, ListContainer, ListName, Overlay } from "./style";
-import add from '../../../assets/icons/add.png';
-import { StatusLabel } from "../style";
+import { Content, Header, ListContainer, ListName, Overlay } from "./style";
+
+import { useContext, useEffect, useRef, useState } from "react";
+import { SetJWT } from "../../../store/context";
+import {
+  GetFavouriteLists,
+  AddPropertyToFavouriteList,
+  CreateNewFavList,
+} from "../../../services/FavoritesService.js";
 
 const Popup = (props) => {
+  const { jwt } = useContext(SetJWT);
+  const [favoriteLists, setFavoriteLists] = useState([]);
+  const [flag, setFlag] = useState([]);
 
-    const closeFunc = (e) => {
-        e.preventDefault();
-        props.onData(false);
-    }
+  const propertyId = 5;
 
-    return (<Overlay>
-        <Content>
-            <Header>
-                <h2>Favorit List</h2>
-                <Button onClick={(e) => closeFunc(e)}>Close</Button>
-            </Header>
-            <ListContainer>
-                <Button >Add</Button>
-                <ListName>
-                    Fav
-                </ListName>
-            </ListContainer>
-            <Header>
-                <Input type="text" style={{ width: '20rem' }} placeholder="Add a new List" />
-                <Button>Add new list</Button>
-            </Header>
-        </Content>
-    </Overlay>)
-}
+  const newListRef = useRef("");
+
+  useEffect(() => {
+    GetFavouriteLists({ jwt })
+      .then((res) => {
+        setFavoriteLists(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [flag]);
+
+  const closeFunc = (e) => {
+    e.preventDefault();
+    props.onData(false);
+  };
+
+  const HandleAddToListClick = (e, listId) => {
+    e.preventDefault();
+
+    AddPropertyToFavouriteList({ jwt }, listId, propertyId)
+      .then((res) => {})
+      .catch((err) => console.log(err));
+
+    closeFunc(e);
+    props.onData(false);
+  };
+
+  const HandleCreateNewFavListClick = (e) => {
+    e.preventDefault();
+
+    CreateNewFavList({ jwt }, newListRef.current.value)
+      .then((res) => {
+        newListRef.current.value = "";
+        setFlag(!flag);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <Overlay>
+      <Content>
+        <Header>
+          <h2>Favorit List</h2>
+          <Button onClick={(e) => closeFunc(e)}>Close</Button>
+        </Header>
+        <ListContainer style={{ display: "flex", flexDirection: "column" }}>
+          {favoriteLists.map((l) => {
+            return (
+              <div>
+                <ListName>{l.name + "     "}</ListName>
+                <Button onClick={(e) => HandleAddToListClick(e, l.id)}>
+                  Add to this list
+                </Button>
+              </div>
+            );
+          })}
+        </ListContainer>
+        <Header>
+          <Input
+            ref={newListRef}
+            type="text"
+            style={{ width: "20rem" }}
+            placeholder="Add a new List"
+          />
+          <Button onClick={(e) => HandleCreateNewFavListClick(e)}>
+            Add new list
+          </Button>
+        </Header>
+      </Content>
+    </Overlay>
+  );
+};
 
 export default Popup;
